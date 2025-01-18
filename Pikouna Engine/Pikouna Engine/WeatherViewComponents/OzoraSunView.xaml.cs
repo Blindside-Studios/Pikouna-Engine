f79987c45727429f3c70e17addf6ffbead47a674
Blindside Studios <64647061+Blindside-Studios@users.noreply.github.com>
2025-01-19 00:40:28 +0100

    Implemented Ozora Physics Sim
    The sun simulation is working. It is not very performant, however, not because of the simulation itself (60 vs 240hz don't make any difference), it's because of the way WinUI 3 draws images. That means we have to rely on another framework to handle drawing for us. Stay tuned.

---
+++ Pikouna Engine/Pikouna Engine/WeatherViewComponents/OzoraSunView.xaml.cs
@@ -0,0 +1,114 @@
+using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Controls.Primitives;
+using Microsoft.UI.Xaml.Data;
+using Microsoft.UI.Xaml.Input;
+using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Navigation;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Ozora;
+using System.Diagnostics;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace Pikouna_Engine.WeatherViewComponents
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class OzoraSunView : Page
+    {
+        OzoraEngine Ozora = new OzoraEngine();
+
+        public OzoraSunView()
+        {
+            this.InitializeComponent();
+            this.NavigationCacheMode = NavigationCacheMode.Disabled;
+            this.Loaded += PhysicsSunSimulation_Loaded;
+            this.Unloaded += PhysicsSunSimulation_Unloaded;
+        }
+
+        private void PhysicsSunSimulation_Unloaded(object sender, RoutedEventArgs e)
+        {
+            Ozora.Physics.MouseCursorEngaged = false;
+            Ozora.Physics.InterruptSimulation();
+            Ozora = null;
+            //Debug.WriteLine("Unloaded Ozora Sun Simulation Model");
+        }
+
+        private void PhysicsSunSimulation_Loaded(object sender, RoutedEventArgs e)
+        {
+            Ozora = new OzoraEngine();
+
+            OzoraSettings SunSettings = new OzoraSettings()
+            {
+                SimulationStyle = SimulationStyle.Sun,
+                FrameRate = 60,
+                MaxVectorDeltaPerFrame = 1.5f,
+                RubberBandingModifier = 0.2f,
+                EnableBorderCollision = true,
+                EnableBounceOnCollision = true,
+                BounceMomentumRetention = 0.8f,
+                TrailingDragCoefficient = 0.01f,
+                TrailingType = TrailingType.Vector
+            };
+
+            Ozora.Physics.Interface = new OzoraInterface()
+            {
+                ObjectWidth = (float)SunObject.ActualWidth,
+                ObjectHeight = (float)SunObject.ActualHeight,
+                Settings = SunSettings,
+                AreaDimensions = new Windows.Foundation.Point(SunGrid.ActualWidth, SunGrid.ActualHeight)
+            };
+
+            Ozora.Physics.ObjectPositionCalculated += Physics_ObjectPositionCalculated;
+            OzoraViewModel.Instance.PropertyChanged += MouseViewModel_PropertyChanged;
+
+            Ozora.Physics.StartSimulation();
+            Ozora.Physics.MouseCursorEngaged = true;
+        }
+
+        private void MouseViewModel_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            if (Ozora != null)
+            {
+                Ozora.Physics.Interface.PointerLocation = OzoraViewModel.Instance.MousePosition;
+                Ozora.Physics.MouseCursorEngaged = OzoraViewModel.Instance.MouseEngaged;
+            }
+        }
+
+        private void Physics_ObjectPositionCalculated(object sender, ObjectPositionUpdatedEvent e)
+        {
+            DispatcherQueue.TryEnqueue(() =>
+            {
+                this.SunObject.Translation = e.NewTranslationVector;
+                if (SunObject.Translation.Y > SunGrid.ActualHeight * 0.33)
+                {
+                    double nightFactor = Math.Clamp((SunObject.Translation.Y - (SunGrid.ActualHeight * 0.33)) / (SunGrid.ActualHeight * 0.5), 0, 1);
+                    double expoNightFactor = Math.Pow(2, nightFactor) - 1;
+                    NightSky.Opacity = expoNightFactor;
+                    EveningSky.Opacity = Math.Clamp(nightFactor * 2, 0, 1);
+                }
+            });
+        }
+
+        private void SunGrid_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            /// Null check as this event is fired when the page loads, 
+            /// briefly before the code in the PageLoaded event is executed, 
+            /// which initializes the Interface
+            if (Ozora.Physics.Interface != null)
+            {
+                Ozora.Physics.Interface.AreaDimensions =
+                    new Windows.Foundation.Point(SunGrid.ActualWidth, SunGrid.ActualHeight);
+            }
+        }
+    }
+}
--- Pikouna Engine/Pikouna Interface/MainWindow.xaml.cs
+++ Pikouna Engine/Pikouna Interface/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using Microsoft.UI.Xaml.Media;
 using Microsoft.UI.Xaml.Navigation;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Pikouna_Engine;
+using System.Diagnostics;
 
 // To learn more about WinUI, the WinUI project structure,
 // and more about our project templates, see: http://aka.ms/winui-project-info.
@@ -26,11 +28,25 @@ namespace Pikouna_Interface
         public MainWindow()
         {
             this.InitializeComponent();
+            ContentFrame.NavigateToType(typeof(Pikouna_Engine.WeatherView), null, null);
         }
 
-        private void myButton_Click(object sender, RoutedEventArgs e)
+        private void EverythingGrid_PointerMoved(object sender, PointerRoutedEventArgs e)
         {
-            myButton.Content = "Clicked";
+            OzoraViewModel.Instance.MousePosition = e.GetCurrentPoint((UIElement)sender).Position;
+            Debug.WriteLine("mouse pointer at " + e.GetCurrentPoint((UIElement)sender).Position);
+        }
+
+        private void EverythingGrid_PointerEntered(object sender, PointerRoutedEventArgs e)
+        {
+            OzoraViewModel.Instance.MouseEngaged = true;
+            Debug.WriteLine("pointer entered");
+        }
+
+        private void EverythingGrid_PointerExited(object sender, PointerRoutedEventArgs e)
+        {
+            OzoraViewModel.Instance.MouseEngaged = false;
+            Debug.WriteLine("pointer exited");
         }
     }
 }

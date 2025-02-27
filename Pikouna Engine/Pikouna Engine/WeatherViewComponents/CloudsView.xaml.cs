e4591db9df6034b0e6470b2f6c51ef539ea1d898
Blindside Studios <64647061+Blindside-Studios@users.noreply.github.com>
2025-02-27 03:10:09 +0100

    Experimental cloud rendering
    We can now render clouds! Just use the slider or the view model variable to determine how many clouds can be rendered!

--- Pikouna Engine/Pikouna Engine/WeatherViewComponents/CloudsView.xaml.cs
+++ Pikouna Engine/Pikouna Engine/WeatherViewComponents/CloudsView.xaml.cs
@@ -1,3 +1,4 @@
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -10,6 +11,8 @@ using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Numerics;
+using System.Runtime.InteropServices;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
@@ -19,25 +22,217 @@ using Windows.Foundation.Collections;
 
 namespace Pikouna_Engine.WeatherViewComponents
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
     public sealed partial class CloudsView : Page
     {
+        List<CloudMainEntity> Clouds = new List<CloudMainEntity> { };
+        
         public CloudsView()
         {
             this.InitializeComponent();
+            this.Loaded += CloudsView_Loaded;
             WeatherViewModel.Instance.PropertyChanged += Instance_PropertyChanged;
+        }
+
+        private void CloudsView_Loaded(object sender, RoutedEventArgs e)
+        {
+            WeatherViewModel.Instance.CloudCover = 50;
+            for (int i = 0; i < 10; i++)
+            {
+                Clouds.Add(CloudMainEntity.RequestNewCloud(CloudsCanvas.ActualHeight * CloudsCanvas.ActualWidth));
+            }
         }
 
         private void Instance_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
             // add logic to handle updated cloud cover here
+            if (WeatherViewModel.Instance.CloudCover != 0 && Clouds.Count == 0)
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Clouds.Add(CloudMainEntity.RequestNewCloud(CloudsCanvas.ActualHeight * CloudsCanvas.ActualWidth));
+                }
+            }
+            else if (WeatherViewModel.Instance.CloudCover == 0 && Clouds.Count != 0) Clouds.Clear();
+            else if (Clouds.Count() > 0) foreach (var cloud in Clouds) cloud.ManageProperties(CloudsCanvas.ActualHeight * CloudsCanvas.ActualWidth);
+
+            if (WeatherViewModel.Instance.CloudCover == 100) CloudsCanvas.ClearColor = Colors.White;
+            else CloudsCanvas.ClearColor = Colors.Transparent;
+
+            CloudsCanvas.Invalidate();
         }
 
         private void CloudsCanvas_Draw(Microsoft.Graphics.Canvas.UI.Xaml.CanvasControl sender, Microsoft.Graphics.Canvas.UI.Xaml.CanvasDrawEventArgs args)
         {
+            var ds = args.DrawingSession;
+            //ds.Clear(Colors.SkyBlue);
 
+            if (Clouds.Count() > 0)
+            {
+                foreach (var cloud in Clouds)
+                {
+                    var renderObjects = cloud.getObjectsToRender((float)CloudsCanvas.ActualWidth, (float)CloudsCanvas.ActualHeight);
+
+                    foreach (var obj in renderObjects)
+                    {
+                        var color = Colors.White;
+                        color.R = color.G = (byte)(255 - (5 * obj.RenderHierarchy));
+                        ds.FillCircle(obj.Translation, (float)obj.Radius, color);
+                    }
+                }
+            }
         }
+
+        private void CloudsCanvas_SizeChanged(object sender, SizeChangedEventArgs e)
+        {
+            Debug.WriteLine(CloudsCanvas.ActualHeight * CloudsCanvas.ActualWidth);
+            if (Clouds.Count() > 0)
+            {
+                foreach (var cloud in Clouds) cloud.ManageProperties(CloudsCanvas.ActualHeight * CloudsCanvas.ActualWidth);
+            }
+            CloudsCanvas.Invalidate();
+        }
+    }
+
+    class CloudMainEntity
+    {
+        public static CloudMainEntity RequestNewCloud(double AreaSize)
+        {
+            Random rnd = new Random();
+            var cloud = new CloudMainEntity
+            {
+                Translation = new Vector2((float)rnd.NextDouble(), (float)rnd.NextDouble()),
+                MovementSpeed = 0
+            };
+            cloud.ManageProperties(AreaSize);
+            return cloud;
+        }
+
+        public void ManageProperties(double AreaSize)
+        {
+            var cloudCover = WeatherViewModel.Instance.CloudCover;
+            var attachments = Convert.ToInt32(Math.Round(cloudCover / 20)) + 2;
+
+            if (cloudCover > 0) this.Radius = cloudCover * (AreaSize / 500000);
+            else this.Radius = 0;
+
+            if (AttachedClouds == null) AttachedClouds = new List<CloudAttachmentBlob>();
+
+            while (AttachedClouds.Count() > attachments) AttachedClouds.RemoveAt(0);
+            
+            if (AttachedClouds.Count() < attachments)
+            {
+                while (AttachedClouds.Count() < attachments)
+                {
+                    AttachedClouds.Add(CloudAttachmentBlob.GenerateAttachment(1, this.Radius));
+                }
+            }
+            foreach (var cloud in AttachedClouds)
+            {
+                cloud.ParentRadius = this.Radius;
+                cloud.ManageProperties();
+            }
+        }
+
+        public List<CloudRenderObject> getObjectsToRender(float CanvasWidth, float CanvasHeight)
+        {
+            var list = new List<CloudRenderObject>();
+            var translation = new Vector2(this.Translation.X * CanvasWidth, this.Translation.Y * CanvasHeight);
+            list.Add(new CloudRenderObject()
+            {
+                Radius = this.Radius,
+                Translation = translation,
+                RenderHierarchy = 0
+            });
+            foreach(var cloud in AttachedClouds)
+            {
+                list.AddRange(cloud.contributeRenderAssets(translation));
+            }
+            list = list.OrderBy(o => o.RenderHierarchy).ToList();
+            return list;
+        }
+
+        public double Radius { get; set; }
+        public Vector2 Translation { get; set; }
+        public double MovementSpeed { get; set; }
+        public List<CloudAttachmentBlob> AttachedClouds { get; set; }
+    }
+
+    class CloudAttachmentBlob
+    {
+        public static CloudAttachmentBlob GenerateAttachment(int hierarchyDepth, double parentRadius) 
+        {
+            Random rnd = new Random();
+            var cloudCover = WeatherViewModel.Instance.CloudCover;
+            var cloud = new CloudAttachmentBlob()
+            {
+                HierarchyDepth = hierarchyDepth,
+                ParentRadius = parentRadius,
+                AttachmentAngle = rnd.NextDouble() * 2 * Math.PI,
+                AngularMovementSpeed = 0,
+                AttachedClouds = new List<CloudAttachmentBlob>() { },
+                ChildGenerationVariance = rnd.NextDouble() + 0.5
+            };
+            cloud.ManageProperties();
+            return cloud;
+        }
+
+        public void ManageProperties()
+        {
+            var cloudCover = WeatherViewModel.Instance.CloudCover;
+            this.Radius = this.ParentRadius * 0.66;
+            if (HierarchyDepth < (cloudCover / 25))
+            {
+                var attachments = Convert.ToInt32(Math.Round(cloudCover / 33 * this.ChildGenerationVariance));
+                if (AttachedClouds == null) AttachedClouds = new List<CloudAttachmentBlob>();
+                while (AttachedClouds.Count() > attachments) AttachedClouds.RemoveAt(0);
+                if (AttachedClouds.Count() < attachments)
+                {
+                    while (AttachedClouds.Count() < attachments)
+                    {
+                        AttachedClouds.Add(CloudAttachmentBlob.GenerateAttachment(this.HierarchyDepth + 1, this.Radius));
+                    }
+                }
+                foreach (var cloud in AttachedClouds)
+                {
+                    cloud.ParentRadius = this.Radius;
+                    cloud.ManageProperties();
+                }
+            }
+            else AttachedClouds.Clear();
+        }
+
+        public List<CloudRenderObject> contributeRenderAssets(Vector2 parentTranslation)
+        {
+            var list = new List<CloudRenderObject>();
+            var translation = new Vector2(
+                (float)(Math.Cos(this.AttachmentAngle) * ParentRadius + parentTranslation.X), 
+                (float)(Math.Sin(this.AttachmentAngle) * ParentRadius + parentTranslation.Y));
+            list.Add(new CloudRenderObject()
+            {
+                Radius = this.Radius,
+                Translation = translation,
+                RenderHierarchy = this.HierarchyDepth,
+            });
+            foreach (var cloud in AttachedClouds)
+            {
+                list.AddRange(cloud.contributeRenderAssets(translation));
+            }
+            return list;
+        }
+
+        public int HierarchyDepth { get; set; }
+        public double ParentRadius { get; set; }
+        public double Radius { get; set; }
+        public double AttachmentAngle { get; set; }
+        public double AngularMovementSpeed { get; set; }
+        public List<CloudAttachmentBlob> AttachedClouds { get; set; }
+        public double ChildGenerationVariance { get; set; }
+    }
+
+    class CloudRenderObject
+    {
+        public double Radius { get; set; }
+        public Vector2 Translation { get; set; }
+        public int RenderHierarchy { get; set; }
     }
 }

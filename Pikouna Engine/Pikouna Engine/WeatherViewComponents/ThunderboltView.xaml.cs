8c44e1ddaebf77ed5778e522c4a0a09af642eff7
Blindside Studios <64647061+Blindside-Studios@users.noreply.github.com>
2025-03-10 14:11:44 +0100

    Lightning strike animation
    Lightning strikes now animate! As always, it respects when you have animation effects disabled in Windows and it also respects when the window doesn't have focus, not animating as to avoid distracting you.

--- Pikouna Engine/Pikouna Engine/WeatherViewComponents/ThunderboltView.xaml.cs
+++ Pikouna Engine/Pikouna Engine/WeatherViewComponents/ThunderboltView.xaml.cs
@@ -1,4 +1,7 @@
+using Microsoft.Graphics.Canvas;
+using Microsoft.Graphics.Canvas.Effects;
 using Microsoft.Graphics.Canvas.Geometry;
+using Microsoft.UI;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Controls.Primitives;
@@ -14,6 +17,7 @@ using System.IO;
 using System.Linq;
 using System.Numerics;
 using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
 using Windows.UI.ViewManagement;
@@ -31,6 +35,14 @@ namespace Pikouna_Engine.WeatherViewComponents
         List<Vector2> _dotMatrix = new List<Vector2>();
         List<LightningBoltPiece> _lightningBolt = new();
 
+        // animation variables
+        private bool _isRenderingStrike = false;
+        private bool _isRenderingStrikeDetails = true;
+        private double _pathfindingProgress = 0;
+        private float _strikeBloomModifier = 1;
+
+        DispatcherTimer _thunderAnimationTimer;
+
         public ThunderboltView()
         {
             this.InitializeComponent();
@@ -42,17 +54,23 @@ namespace Pikouna_Engine.WeatherViewComponents
         {
             if (e.PropertyName == nameof(WeatherViewModel.Instance.WeatherType))
             {
-                var weather = WeatherViewModel.Instance.WeatherType;
-                if (weather == WeatherType.ThunderstormSlightOrModerate || weather == WeatherType.ThunderstormWithHailSlight || weather == WeatherType.ThunderstormWithHailHeavy)
-                {
-                    startAnimating();
-                }
-                else
-                {
-                    _dotMatrix.Clear();
-                    _lightningBolt.Clear();
-                    LightningBoltCanvas.Invalidate();
-                }
+                Initiate();
+            }
+        }
+
+        private void Initiate()
+        {
+            var weather = WeatherViewModel.Instance.WeatherType;
+            var isCorrectWeather = weather == WeatherType.ThunderstormSlightOrModerate || weather == WeatherType.ThunderstormWithHailSlight || weather == WeatherType.ThunderstormWithHailHeavy;
+            if (isCorrectWeather)
+            {
+                startAnimating();
+            }
+            else
+            {
+                _dotMatrix.Clear();
+                _lightningBolt.Clear();
+                LightningBoltCanvas.Invalidate();
             }
         }
 
@@ -67,7 +85,83 @@ namespace Pikouna_Engine.WeatherViewComponents
 
         private void startAnimating()
         {
+            _isRenderingStrike = false;
+            _isRenderingStrikeDetails = true;
+            _pathfindingProgress = 0;
+            _strikeBloomModifier = 1;
+            handleAnimations();
             GenerateLightningBolt();
+            playAnimation();
+        }
+
+        private void handleAnimations()
+        {
+            if (_thunderAnimationTimer != null) _thunderAnimationTimer.Stop();
+            _thunderAnimationTimer = new DispatcherTimer();
+            _thunderAnimationTimer.Interval = TimeSpan.FromMilliseconds(10000); // Fires every 20 seconds
+            _thunderAnimationTimer.Tick += _thunderAnimationTimer_Tick;
+            if (ApplicationViewModel.Instance.AreAnimationsPlaying) _thunderAnimationTimer.Start();
+        }
+
+        private void _thunderAnimationTimer_Tick(object sender, object e)
+        {
+            GenerateLightningBolt();
+            playAnimation();
+        }
+
+        private async void playAnimation()
+        {
+            UISettings settings = new UISettings();
+            if (settings.AnimationsEnabled && ApplicationViewModel.Instance.CanPlayAnimations)
+            {
+                // start values
+                _isRenderingStrike = false;
+                _isRenderingStrikeDetails = true;
+                _pathfindingProgress = 0;
+                _strikeBloomModifier = 1;
+
+                // pathfinding animation to the floor
+                _isRenderingStrike = true;
+                while (_pathfindingProgress < 0.9)
+                {
+                    _pathfindingProgress += 0.075;
+                    LightningBoltCanvas.Invalidate();
+                    await Task.Delay(2);
+                }
+
+                while (_strikeBloomModifier < 10)
+                {
+                    _strikeBloomModifier += 2;
+                    LightningBoltCanvas.Invalidate();
+                    await Task.Delay(1);
+                }
+                _isRenderingStrikeDetails = false;
+
+                while (_strikeBloomModifier > 5)
+                {
+                    _strikeBloomModifier -= 0.1f;
+                    LightningBoltCanvas.Invalidate();
+                    await Task.Delay(2);
+                }
+                while (_strikeBloomModifier < 7)
+                {
+                    _strikeBloomModifier += 0.2f;
+                    LightningBoltCanvas.Invalidate();
+                    await Task.Delay(2);
+                }
+                while (_strikeBloomModifier > 1)
+                {
+                    _strikeBloomModifier -= 0.1f;
+                    LightningBoltCanvas.Invalidate();
+                    await Task.Delay(2);
+                }
+                while (_strikeBloomModifier > 0)
+                {
+                    _strikeBloomModifier -= 0.01f;
+                    LightningBoltCanvas.Invalidate();
+                    await Task.Delay(3);
+                }
+            }
         }
 
         private void GenerateLightningBolt()
@@ -173,25 +267,65 @@ namespace Pikouna_Engine.WeatherViewComponents
                 EndCap = CanvasCapStyle.Round
             };
             Vector2 screenDimensions = new Vector2((float)LightningBoltCanvas.ActualWidth, (float)LightningBoltCanvas.ActualHeight);
-            foreach (var piece in _lightningBolt)
+            if (_isRenderingStrike)
             {
-                var thickness = 12;
-                if (!piece.IsInMainBolt)
+                foreach (var piece in _lightningBolt)
                 {
-                    thickness = 5 - piece.StrayFromMainDepth / 6;
+                    var renderBolt = true;
+                    float thickness = 12;
+                    if (!piece.IsInMainBolt && piece.EndPoint.Y < _pathfindingProgress)
+                    {
+                        thickness = 5 - piece.StrayFromMainDepth / 6;
+                    }
+                    else if (piece.EndPoint.Y > _pathfindingProgress)
+                    {
+                        renderBolt = false;
+                    }
+
+                    if (piece.IsInMainBolt) thickness = thickness * _strikeBloomModifier;
+                    else if (!_isRenderingStrikeDetails) renderBolt = false;
+
+                    if (renderBolt) {
+                        ds.DrawLine(
+                            new Vector2(piece.StartPoint.X * screenDimensions.X, piece.StartPoint.Y * screenDimensions.Y),
+                            new Vector2(piece.EndPoint.X * screenDimensions.X, piece.EndPoint.Y * screenDimensions.Y),
+                            Microsoft.UI.Colors.LightYellow,
+                            thickness,
+                            strokeStyle);
+                    }
                 }
 
-                ds.DrawLine(
-                    new Vector2(piece.StartPoint.X * screenDimensions.X, piece.StartPoint.Y * screenDimensions.Y),
-                    new Vector2(piece.EndPoint.X * screenDimensions.X, piece.EndPoint.Y * screenDimensions.Y),
-                    Microsoft.UI.Colors.LightYellow, 
-                    thickness, 
-                    strokeStyle);
+                // render bloom as required by rendering into an off-screen area
+                if (_strikeBloomModifier > 1)
+                {
+                    CanvasRenderTarget lightningTarget = new CanvasRenderTarget(sender, (float)LightningBoltCanvas.ActualWidth, (float)LightningBoltCanvas.ActualHeight, 96);
+                    using (var _ds = lightningTarget.CreateDrawingSession())
+                    {
+                        _ds.Clear(Colors.Transparent);
+                        foreach (var piece in _lightningBolt)
+                        {
+                            if (piece.IsInMainBolt)
+                            {
+                                float thickness = 12;
+
+                                _ds.DrawLine(
+                                        new Vector2(piece.StartPoint.X * screenDimensions.X, piece.StartPoint.Y * screenDimensions.Y),
+                                        new Vector2(piece.EndPoint.X * screenDimensions.X, piece.EndPoint.Y * screenDimensions.Y),
+                                        Microsoft.UI.Colors.LightYellow,
+                                        thickness * (_strikeBloomModifier - 1) * 10,
+                                        strokeStyle);
+                            }
+                        }
+                    }
+                    var blurredLightning = new GaussianBlurEffect
+                    {
+                        Source = lightningTarget,
+                        BlurAmount = _strikeBloomModifier * 15,
+                        BorderMode = EffectBorderMode.Hard
+                    };
+                    ds.DrawImage(blurredLightning);
+                }
             }
-            /*foreach (var dot in _dotMatrix)
-            {
-                ds.FillCircle(new Vector2(dot.X * screenDimensions.X, dot.Y * screenDimensions.Y), 1, Microsoft.UI.Colors.Red);
-            }*/
         }
     }
 
